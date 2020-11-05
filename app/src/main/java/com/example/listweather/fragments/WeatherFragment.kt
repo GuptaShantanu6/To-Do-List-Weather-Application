ea9882378750d3677465624c5fe203c382885359
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2020-11-05 18:53:20 +0530

    Weather: Initial Layout

--- app/src/main/java/com/example/listweather/fragments/WeatherFragment.kt
+++ app/src/main/java/com/example/listweather/fragments/WeatherFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listweather.R
+import kotlinx.android.synthetic.main.fragment_weather.*
 
 class WeatherFragment : Fragment() {
 
